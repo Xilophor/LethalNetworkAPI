0f49b614b1413c1d0e2b08ec460908b2212bb089
Xilophor <ryanzgreg@gmail.com>
2024-01-08 16:11:37 -0500

    remove unnecessary files

--- LethalNetworkAPI/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Global using directives
-
-global using System;
-global using System.Linq;
-global using System.Reflection;
-global using LethalNetworkAPI.Networking;
-global using Unity.Netcode;
-global using UnityEngine;

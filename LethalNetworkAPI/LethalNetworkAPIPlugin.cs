edae94b6244b87190816839ad74d208231d5a1cf
Xilophor <ryanzgit@pm.me>
2024-06-14 10:32:56 -0400

    Add Lobby Compatibility req; Remove NetcodePatcher

--- LethalNetworkAPI/LethalNetworkAPIPlugin.cs
+++ LethalNetworkAPI/LethalNetworkAPIPlugin.cs
@@ -1,54 +1,33 @@
-using System;
-using System.Reflection;
+namespace LethalNetworkAPI;
+
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using LethalNetworkAPI.Old.Networking;
-using UnityEngine;
-
-namespace LethalNetworkAPI;
-
+using LobbyCompatibility.Attributes;
+using LobbyCompatibility.Enums;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[LobbyCompatibility(CompatibilityLevel.ServerOnly, VersionStrictness.Minor)]
 public class LethalNetworkAPIPlugin : BaseUnityPlugin
 {
+    public static LethalNetworkAPIPlugin Instance { get; private set; } = null!;
+
+    internal static new ManualLogSource Logger { get; private set; } = null!;
+
+    private static Harmony _harmony = null!;
+
     private void Awake()
     {
 #if NET472
-        throw new Exception("The incorrect version of LethalNetworkAPI is installed. Use the netstandard2.1 version provided by the Thunderstore listing or the GitHub release. The currently installed version will *not* work as intended.");
+        throw new System.Exception("The incorrect version of LethalNetworkAPI is installed. Use the netstandard2.1 version provided by the Thunderstore listing or the GitHub release. The currently installed version will *not* work as intended.");
 #endif
         Instance = this;
         Logger = base.Logger;
-        
+
         _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-        
-        NetcodePatcher();
-        
-        _harmony.PatchAll(typeof(NetworkObjectManager));
+
+        _harmony.PatchAll();
 
         Logger.LogInfo($"LethalNetworkAPI v{MyPluginInfo.PLUGIN_VERSION} has Loaded.");
     }
-
-    private static void NetcodePatcher()
-    {
-        var types = Assembly.GetExecutingAssembly().GetTypes();
-        foreach (var type in types)
-        {
-            var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-            foreach (var method in methods)
-            {
-                var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
-                if (attributes.Length > 0)
-                {
-                    method.Invoke(null, null);
-                }
-            }
-        }
-    }
-    
-    public static LethalNetworkAPIPlugin Instance = null!;
-    
-    internal new static ManualLogSource Logger { get; private set; } = null!;
-    
-    private static Harmony _harmony = null!;
 }

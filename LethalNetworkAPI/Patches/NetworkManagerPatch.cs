8f0db50a5bdaf0713fd8a9c8aada4576b0ac565e
Xilophor <ryanzgit@pm.me>
2024-06-14 10:33:16 -0400

    Change Patching Method

---
+++ LethalNetworkAPI/Patches/NetworkManagerPatch.cs
@@ -0,0 +1,28 @@
+namespace LethalNetworkAPI.Patches;
+
+using HarmonyLib;
+using Internal;
+using Old.Networking;
+using Unity.Netcode;
+
+[HarmonyPatch(typeof(NetworkManager))]
+[HarmonyPriority(Priority.HigherThanNormal)]
+[HarmonyWrapSafe]
+internal static class NetworkManagerPatch
+{
+    [HarmonyPatch(nameof(NetworkManager.Initialize))]
+    [HarmonyPostfix]
+    public static void InitializePatch()
+    {
+        _ = new UnnamedMessageHandler();
+        _ = new NetworkHandler();
+    }
+
+    [HarmonyPatch(nameof(NetworkManager.Shutdown))]
+    [HarmonyPrefix]
+    public static void ShutdownPatch()
+    {
+        UnnamedMessageHandler.Instance?.Dispose();
+        NetworkHandler.Instance?.Dispose();
+    }
+}
